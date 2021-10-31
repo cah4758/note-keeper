6fb944b171f458d14474a25e9d9d0624845e64e3
Charles Hernandez <charlesh4758@gmail.com>
2021-10-31 11:44:36 -0500

    Port edits with heroku

--- express.js
+++ express.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const api = require('./routes/index.js');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -23,5 +23,5 @@ app.get('/*', (req, res) =>
 );
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at ${PORT} 🚀`)
 );
