4744dc132ac103396459970328c800fc2e8f3eac
Charles Hernandez <charlesh4758@gmail.com>
2021-10-30 20:34:23 -0500

    Created notes API route with get & post requests

--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,34 @@
 const notes = require('express').Router();
+const path = require('path');
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const { v4: uuidv4 } = require('uuid');
 
 // API ROUTES
-// get "/api/notes" should read db.json and return saved notes as JSON
-notes.get('/notes', (req, res) => {
+// get "/api/notes/" should read db.json and return saved notes as JSON
+notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// post "/api/notes" should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (add UUID to post)
+// post "/api/notes/" should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. Add UUID to post
+notes.post('/', (req, res) => {
+    console.log(req.body);
+  
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        note_id: uuidv4(),
+      };
+
+      readAndAppend(newNote, './db/db.json');
+      res.json(`Note has been added! Way to keep track of stuff!`);
+
+      res.sendFile(path.join(__dirname, '../public/notes.html'));
+    } else {
+      res.error('Error in adding note. Figure it out!');
+    }
+  });
+
 module.exports = notes;
